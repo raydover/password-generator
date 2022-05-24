579dc3a6a409fd4394c3122e7bfc9b31c6b05740
raydover <105178034+raydover@users.noreply.github.com>
2022-05-24 15:25:18 -0400

    variable choice, question uppercase working JS

--- script.js
+++ script.js
@@ -4,22 +4,33 @@ var generateBtn = document.querySelector("#generate");
 // Instructor Provided: Anthony Cooper
 function generatePassword() {
   var lowercase = "abcdefghijklmnopqrstuvwxyz";
-  var numbers = "0123456789";
-  var special = "#$%&()*+,-./:;<=>?";
   var uppercase = lowercase.toUpperCase();
-
+  var special = "#$%&()*+,-./:;<=>?";
+  var numbers = "0123456789";
+  var choice = "";
+  
   var input = parseInt(prompt("Enter a length between 8 and 128 characters"));
   // Password lenth & Invalid message
   if (input >= 8 && input <= 128) {
   } else {
     alert("INVALID ENTRY! Try Again.");
   }
+
   // Enter Good Code Here
   // Question:lowercase
   var lowercase = confirm ("Would you like to use lowercase letter?");
   if (lowercase) {
-    possible += lowercase
+    choice += lowercase
   }
+
+   // Question:lowercase
+   var uppercase = confirm ("Would you like to use lowercase letter?");
+   if (upppercase) {
+     choice += upppercase
+   }
+
+
+  
 
 
 
